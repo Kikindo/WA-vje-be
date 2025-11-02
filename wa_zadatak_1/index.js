1682f73be25451485007f87783233b7995625d5f
Kikindo <tomislav.rakuljic@gmail.com>
2025-11-02 13:59:30 +0100

    Rješenje samostalnog zadatka

--- wa_zadatak_1/index.js
+++ wa_zadatak_1/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!');
+    res.sendFile(path.join(__dirname, 'public', 'index.html' ));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, ime: 'Ivan', prezime: 'Anić' },
+        { id: 2, ime: 'Lea', prezime: 'Markić' },
+        { id: 3, ime: 'Iva', prezime: 'Ivić' }
+    ];
+    res.json(users);
 });
 
 app.listen(PORT, error => {
